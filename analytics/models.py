5278d0af3c68c63f47d4a0b8f7b948514acb0dc8
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-04-06 18:24:34 +0530

    Integrating Backend

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class VisitorStats(models.Model):
+    # Field to store the running total of visitors
+    total_visits = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Total Visits: {self.total_visits}"
