55087ca4f9d8fba67dd4f8c3f54e64b2a0db687c
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-07-30 06:36:03 +0530

    System ready Tracker has been built

--- visitor_tracker/core/urls.py
+++ visitor_tracker/core/urls.py
@@ -1,22 +1,18 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# A basic single-page view for local testing
+def home_view(request):
+    return HttpResponse("""
+        <div style='font-family: sans-serif; text-align: center; padding-top: 50px;'>
+            <h1>🚀 Visitor Tracker Test Page</h1>
+            <p>Your visit is being recorded by the custom middleware!</p>
+            <p><a href='/admin/'>Go to Django Admin Panel</a></p>
+        </div>
+    """)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
 ]
