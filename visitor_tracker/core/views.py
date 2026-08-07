2b0c02b3a95bd31a834cc23d363a05686edb201d
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-08-07 13:02:09 +0530

    The login System is back at testing with the tracker system

--- visitor_tracker/core/views.py
+++
@@ -1,8 +0,0 @@
-# core/views.py
-from django.shortcuts import render
-
-def home_view(request):
-    """
-    Renders the main homepage for this project.
-    """
-    return render(request, 'index.html')
