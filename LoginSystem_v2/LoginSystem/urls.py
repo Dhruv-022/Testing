a6d4421aa9d68ecb9a5e79694a9d919552d479e1
Dhruv <dhruv2vaishnav@gmail.com>
2026-08-08 07:05:18 +0000

    Cleared the test repo for learning middleware

--- LoginSystem_v2/LoginSystem/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('secure_auth.urls')),
-    path('visitor-analytics/', include('visitor_analytics.urls')),
-]
